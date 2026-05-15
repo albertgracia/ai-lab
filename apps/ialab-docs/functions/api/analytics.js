d6d4a6c6adc9f573eaaea0e92162de6c397600ea
Albert Gràcia <albertgraciaquintana@gmail.com>
2026-05-15 18:19:02 +0000

    fix: remove Pages Functions, restore direct Service Token fetch with Access CORS

--- apps/ialab-docs/functions/api/analytics.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-    return new Response(JSON.stringify({test: "Function works!", time: Date.now()}), {
-        headers: {"Content-Type": "application/json"}
-    });
-}
--- apps/ialab-docs/functions/api/status.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-    return new Response(JSON.stringify({test: "Status function works!"}), {
-        headers: {"Content-Type": "application/json"}
-    });
-}
